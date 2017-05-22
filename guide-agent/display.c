fc442e57353f00f5cdf2094a6dbdb78e05f31121
Michael Perezous <michael.z.perezous.19@dartmouth.edu>
2017-05-22 19:23:45 -0400

    started display.c with include messages and a header

---
+++ guide-agent/display.c
@@ -0,0 +1,13 @@
+/*
+ * display.c - implements the graphical interface for the Guide Agent
+ * using ncurses. Used to abstract the interface functionality from 
+ * the Guide Agent so that it only need worry about game logic
+ *
+ *
+ * GREP, CS50, May 2017
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+
