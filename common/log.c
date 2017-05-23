d1848cfb879069c08e8749005fb0ed6e55ad79de
Michael Perezous <michael.z.perezous.19@dartmouth.edu>
2017-05-22 23:55:06 -0400

    started log.c file

---
+++ common/log.c
@@ -0,0 +1,22 @@
+/*
+ * log.c - shared function to log activity to a logfile based on given format
+ *
+ *
+ * GREP, CS50, May 2017
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void logMessage(FILE *file, char *message, struct connection *connect)
+{
+	// get timestamp
+	char timestamp[27];
+	time_t clk = time(NULL);
+	sprintf(timestamp, "(%s", ctime(&clk));
+	timestamp[25] = ')';
+
+	
+}
