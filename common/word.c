209b9e8eb25bd68f9d1c2b9b41b6b017de290e0b
Michael Perezous <michael.z.perezous.19@dartmouth.edu>
2017-05-25 13:42:10 -0400

    included word.[ch] in guideagent.c and Makefile to be able to normalize words

---
+++ common/word.c
@@ -0,0 +1,21 @@
+/*
+ * word.c - common functions to modify input strings
+ * 
+ * to be used in indexer and querier
+ *
+ * Michael Perezous, CS50, May 2017
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+void NormalizeWord(char *word) {
+	char c;
+
+	for (int i=0; (c = word[i]) != '\0'; i++) {
+		word[i] = tolower(c);
+	}
+	
+}
